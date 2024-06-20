4234277e2ff3480149a2e85352b46649e0785e30
Abu10thahir7-github <142816135+Abu10thahir7-github@users.noreply.github.com>
2024-06-20 12:34:02 +0530

    many update

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -2,35 +2,34 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const app = express();
-const dotenv = require('dotenv');
-const connectDB = require('./Config/dbConnection');
+const mongoose = require('mongoose')
+require('dotenv').config();
 
-const AllFoodRoute = require('./Route/AllFoodRoute')
-dotenv.config();
+const AllFoodRoute = require('./Route/AllFoodRoute');
+// const { connect } = require('http2');
 
-connectDB();
+
+
 app.use(cors());
-app.use(cors({
-   origin: 'http://localhost:5173'
- }));
- app.use((req, res, next) => {
-   res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
-   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-   next();
- });
+
+ const url ="mongodb://localhost:27017/Foode-Delivery"
+ mongoose.connect(url)
+ .then(() => console.log('MongoDB Connected'))
+     .catch(()=>console.log('MongoDB Not Connected'));
  
+     
 
 app.use('/images', express.static(path.join(__dirname, 'public/images')));
 app.use(express.json());
 
-
+ 
 app.use('/api/allFood',AllFoodRoute)
 
 
 app.get('/', (req, res) => {
    res.send('API is running...');
 })
+
 
 
 const PORT = process.env.PORT || 5000;
